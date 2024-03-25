99006db6d10d1a4ef923d6b0ed441c96c0835047
Jagdish24-uc <jagdish.purti@gmail.com>
2024-03-25 11:33:23 +0530

    commit D1

---
+++ pythonlearn.py
@@ -0,0 +1,14 @@
+""" A grocery store sells a bag of ice for 1.25, and makes 20% profit.
+if sells 500 bags of ice, how much total profit does it make? """
+
+cost_of_ice_bag =1.25
+profit_margin =.2
+number_of_bags = 500
+
+#calculation for perbag profit  
+profit_per_bag = profit_margin * cost_of_ice_bag 
+print(profit_per_bag)
+
+#cal for 500 bag
+total_profit_of_bag = profit_per_bag * number_of_bags
+print(" total profit of bag =" , total_profit_of_bag)
