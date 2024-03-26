79f0bdd6b8cc53ce7715d722d51edb00a2359cec
tintus purti <tintuspurti970@gmail.com>
2024-03-26 11:44:43 +0530

    condition with logical operators

---
+++ condition_with_logi_operators.py
@@ -0,0 +1,33 @@
+my_fev_no=1
+my_least_fev_no =5
+a_neutral_no = 3
+
+#eqaulaity check
+print(3==5)
+print(my_fev_no == 1)
+print(my_fev_no==my_least_fev_no)
+
+#not equality check
+my_fev_no!=a_neutral_no
+a_neutral_no!=3
+
+#greater than check
+my_fev_no>a_neutral_no
+a_neutral_no>my_least_fev_no
+
+#less than check
+my_fev_no<10
+my_least_fev_no<my_fev_no
+
+#greater or equal check 
+my_fev_no>=1
+my_fev_no>=3
+
+#less than equal check 
+3+6<=9
+my_fev_no+a_neutral_no<=3
+
+
+
+
+
