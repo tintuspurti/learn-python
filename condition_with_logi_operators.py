97258f4cc460f4b0d033ce175301510abd61d4b9
tintus purti <tintuspurti970@gmail.com>
2024-03-28 15:29:47 +0530

    condition and logi

--- condition_with_logi_operators.py
+++ condition_with_logi_operators.py
@@ -2,6 +2,7 @@ my_fev_no=1
 my_least_fev_no =5
 a_neutral_no = 3
 
+#OPERATOR LOGIC
 #eqaulaity check
 print(3==5)
 print(my_fev_no == 1)
@@ -27,7 +28,6 @@ my_fev_no>=3
 3+6<=9
 my_fev_no+a_neutral_no<=3
 
-
-
-
-
+cost_of_ice =1.25
+is_ice_bag_expensive= cost_of_ice >=10
+print("is that ice bag expensive? " ,is_ice_bag_expensive)
