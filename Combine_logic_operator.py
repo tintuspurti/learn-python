e036cf91f87f0a90676b1046f4e27763d43c85cd
tintus purti <tintuspurti970@gmail.com>
2024-03-30 11:07:02 +0530

    Combine logic practice

---
+++ Combine_logic_operator.py
@@ -0,0 +1,8 @@
+my_fev_no = 1
+my_least_fev_no =5
+a_neutral_no = 3
+print((2>3 and 4<=5) or not (my_fev_no < 0 and True) )
+
+print (not (True and 0<1) or (False and True))
+
+print(not True and 0< 1 or False and True)
