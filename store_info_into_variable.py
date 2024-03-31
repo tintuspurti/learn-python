e5fdcc19d30cc005298201b61dbbbec20ae89ea6
tintus purti <tintuspurti970@gmail.com>
2024-03-31 21:51:04 +0530

    store info in variable

---
+++ store_info_into_variable.py
@@ -0,0 +1,11 @@
+my_fev_color = "blue"
+print(my_fev_color)
+
+color1 , color2 ,color3 = "red","green","blue"
+print(color1)
+print(color2)
+color4=color5=color6= "meganta"
+print(color5)
+my_fev_color ="pink"
+print(my_fev_color)
+    
