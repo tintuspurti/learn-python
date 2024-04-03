20ce51f3b4a7c0ca22c8255ce0ba5c86e6aadebe
tintus purti <tintuspurti970@gmail.com>
2024-04-03 13:12:56 +0530

    data structure tuple

---
+++ data_structure_tuple.py
@@ -0,0 +1,26 @@
+fruits =('apple','cherry','dates')
+
+print(len(fruits))
+print(fruits[2])
+
+print('dates' in fruits)
+#without paratheses '(' ')' python consider this as tuple
+three_heros = 'salman','ajay','vjay'
+print(three_heros)
+#add comma at the end for single element list
+single_element = 4,
+print(single_element)
+not_tuple =(4)
+print(not_tuple)
+#multiple variable with single statement
+points =(2, 5)
+point_x = point_y = points
+print(point_x)
+print(point_y)
+#convert list into tuple
+tuple(['one','two','three'])
+list(('athos','porthos','aramus'))
+
+a_tuple=23,"hello",False,None,23,27,"hello"
+print(a_tuple)
+print(help(a_tuple.count))
